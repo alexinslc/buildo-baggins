e5dc01509012bb42eacbecfbbe65561ec3f0777c
alexinslc <alexinslc@gmail.com>
2025-05-06 21:26:52 -0600

    add TextNode to HTMLNode and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,6 +19,7 @@ class TestTextNode(unittest.TestCase):
         # Test inequality when text_type is different
         node1 = TextNode("Hello", TextType.TEXT, "http://example.com")
         node2 = TextNode("Hello", TextType.BOLD, "http://example.com")
+        self.assertNotEqual(node1, node2)
 
     def test_textnode_inequality_different_url(self):
         # Test inequality when url is different
@@ -37,6 +38,46 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("Hello", TextType.TEXT, None)
         node2 = TextNode("Hello", TextType.TEXT, "http://example.com")
         self.assertNotEqual(node1, node2)
+
+    def test_text(self):
+         node = TextNode("This is a text node", TextType.TEXT)
+         html_node = text_node_to_html_node(node)
+         self.assertEqual(html_node.tag, None)
+         self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+         node = TextNode("This is bold text", TextType.BOLD)
+         html_node = text_node_to_html_node(node)
+         self.assertEqual(html_node.tag, "b")
+         self.assertEqual(html_node.value, "This is bold text")
+
+    def test_italic(self):
+         node = TextNode("This is italic text", TextType.ITALIC)
+         html_node = text_node_to_html_node(node)
+         self.assertEqual(html_node.tag, "i")
+         self.assertEqual(html_node.value, "This is italic text")
+
+    def test_code(self):
+         node = TextNode("This is code text", TextType.CODE)
+         html_node = text_node_to_html_node(node)
+         self.assertEqual(html_node.tag, "code")
+         self.assertEqual(html_node.value, "This is code text")
+
+    def test_link(self):
+            node = TextNode("This is a link", TextType.LINK, "http://example.com")
+            html_node = text_node_to_html_node(node)
+            self.assertEqual(html_node.tag, "a")
+            self.assertEqual(html_node.value, "This is a link")
+            self.assertEqual(html_node.props["href"], "http://example.com")
+
+    def test_image(self):
+            node = TextNode("This is an image", TextType.IMAGE, "http://example.com/image.png")
+            html_node = text_node_to_html_node(node)
+            self.assertEqual(html_node.tag, "img")
+            self.assertEqual(html_node.value, "")
+            self.assertEqual(html_node.props["src"], "http://example.com/image.png")
+            self.assertEqual(html_node.props["alt"], "This is an image")
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
     TEXT = auto()       # Plain text
@@ -27,3 +28,21 @@ class TextNode:
             f"text_type={self.text_type.name}, "
             f"url='{self.url}')"
         )
+
+# convert the text node to html
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError(f"Unknown text type: {text_node.text_type}")
+    
